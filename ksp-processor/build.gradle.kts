c59f5642aff2a7f0cc2c6fcdffda3c5c9d11d85a
Aleksey Yakovlev <alexey.yakovlef@gmail.com>
2024-02-15 20:01:12 +0700

    added snapshot publishing

--- ksp-processor/build.gradle.kts
+++ ksp-processor/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.21"
     alias(libs.plugins.ksp)
+    `maven-publish`
 }
 
 group = "io.github.lexadiky.ktor-jax-rs"
@@ -19,9 +20,37 @@ dependencies {
     ksp(libs.autoservice.ksp)
 }
 
+java {
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        register("maven", MavenPublication::class) {
+            from(project.components["java"])
+            groupId = "io.github.lexa-diky.ktor-jax-rs"
+            artifactId = "ksp-processor"
+            version = "0.0.5-SNAPSHOT"
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            credentials {
+                username = System.getenv("MAVEN_CENTRAL_USERNAME")
+                password = System.getenv("MAVEN_CENTRAL_PASSWORD")
+            }
+        }
+    }
+}
+
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(17)
 }
